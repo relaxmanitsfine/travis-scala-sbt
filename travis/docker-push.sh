0c968cbdeba8c6cd606dd680e80d846c564b6202
relaxmanitsfine <relaxmanitsfine@gmail.com>
2020-05-28 11:33:48 +1000

    docker login fix

--- travis/docker-push.sh
+++ travis/docker-push.sh
@@ -16,7 +16,7 @@ else
   export DOCKER_BUILD_TAG="${JAVA_TAG}-${TRAVIS_COMMIT::8}"
 fi
 
-docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASS" \
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS" \
   && docker tag "fibber:${DOCKER_BUILD_TAG}" \
      "relaxmanitsfine/fibber:${DOCKER_BUILD_TAG}" \
   && docker push "relaxmanitsfine/fibber:${DOCKER_BUILD_TAG}"
